2b619bfef9791c9bb64ea4295e5d8770533d39b6
mrvon-null <niku200210@gmail.com>
2024-09-21 20:05:33 +0300

    Implemented search across registered users and money subtraction for betting. Needs refinements

--- src/Utils.java
+++ src/Utils.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Utils {
 
-
+    static int totalBet = 0;
 
     public static String askForText(String text) {
         Scanner sc = new Scanner(System.in);
@@ -22,14 +21,37 @@ public class Utils {
 
     public static void printAll() {
         for (int i = 0; i < Registration.racerList.size(); i++) {
-            System.out.println(Registration.racerList.get(i).getName() + " has " + Registration.racerList.get(i).getMoney());
+            System.out.println(Registration.racerList.get(i).getName() + " --- " + Registration.racerList.get(i).getMoney());
         }
     }
 
+    public static void chooseParticipants() {
+        int betAmount = askForInt("Enter bet amount:");
+        String[] names = askForText("Enter Names of users that want to bet:").split(",");
+        for (String name : names) {
+            findPersonByName(Registration.racerList, name).subtractMoney(betAmount);
+            totalBet += betAmount;
+        }
+    }
+
+    public static Racers findPersonByName(List<Racers> users, String name) {
+        for (Racers user : users) {
+            if (user.getName().equalsIgnoreCase(name)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
     public static int menuShowAndPick() {
-        System.out.println("Menu:\n"
-                + "1. Register Racers\n"
-                + "2. Exit\n");
+        System.out.println("""
+                Menu:
+                1. Register Users
+                2. Show Users and Balances
+                3. Place Bets
+                4. Start Race
+                5. Exit
+                """);
 
         return askForInt("");
     }

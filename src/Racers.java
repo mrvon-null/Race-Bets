a0cbad7a8b65b9875222e272843e095431b7f668
mrvon-null <niku200210@gmail.com>
2024-09-21 20:03:53 +0300

    New behaviors for changing name and money

--- src/Racers.java
+++ src/Racers.java
@@ -1,11 +1,8 @@
-import javax.print.attribute.ResolutionSyntax;
-
 public class Racers {
     private String name;
     private int money;
 
     public Racers() {
-
     }
 
     public Racers(String name, int money) {
@@ -13,14 +10,20 @@ public class Racers {
         this.money = money;
     }
 
+    public void subtractMoney(int amount) {
+        this.money = this.money - amount;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
-
     }
 
     public int getMoney() {
         return money;
     }
-
 
 }

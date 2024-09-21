eebc4f7e13caaa021598c3f094f313700577816d
mrvon-null <niku200210@gmail.com>
2024-09-21 16:51:28 +0300

    Working menu for registration and exit.

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,23 @@ public class Main {
     static String adminPass = "12345";
 
     public static void main(String[] args) {
+        int menuMode;
+
         if(Login.adminLogin(adminName,adminPass)){
             System.out.println("Welcome to the System!");
-            for (int i = 0; i < 2; i++) {
-                Racers racer = new Racers(Utils.askForText("Name of User: "), Utils.askForInt("Money the User has: ") );
-                Utils.racerList.add(racer);
+
+
+            while (true) {
+                menuMode = Utils.menuShowAndPick();
+
+                switch (menuMode) {
+                    case 1:
+                        Registration.regRacers(Utils.askForInt("Enter the number of racers to register:"));
+                        break;
+                    case 2:
+                        System.exit(0);
+                }
             }
-            Utils.printAll();
         }
     }
 }

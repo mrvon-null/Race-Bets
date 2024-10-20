539dbe076f9b7b06ec47889a79ba5886ee3dedcf
mrvon-null <niku200210@gmail.com>
2024-10-20 19:49:45 +0300

    massive rework to searching, and winner checking

--- src/Utils.java
+++ src/Utils.java
@@ -1,87 +1,118 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Utils {
-
+    static List<Users> foundUsers = new ArrayList<>();
     static int totalBet = 0;
-    static boolean isReady = false;
 
     public static String askForText(String text) {
         Scanner sc = new Scanner(System.in);
         System.out.print(text);
-        //sc.close();
         return sc.next();
     }
 
     public static int askForInt(String text) {
         Scanner sc = new Scanner(System.in);
         System.out.print(text);
-        //sc.close();
-        return sc.nextInt();
+        try {
+            return sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("Enter an integer");
+        }
+        return 0;
     }
 
     public static void printAll() {
-        for (int i = 0; i < Registration.racerList.size(); i++) {
-            System.out.println(Registration.racerList.get(i).getName() + " --- " + Registration.racerList.get(i).getMoney());
+        for (Users user : Registration.userTable.values()) {
+            System.out.println(user.getName() + " --- " + user.getMoney());
         }
     }
 
-    public static String[] chooseParticipants() {
-        if (isReady) {
-            System.out.println("Bets have already been placed. Start the race!");
-            return null;
-        }
-        int betAmount = askForInt("Enter bet amount:");
+    public static boolean pickUsersToBet() {
         String[] names = askForText("Enter Names of users that want to bet:").split(",");
-        for (String name : names) {
-            findPersonByName(Registration.racerList, name).subtractMoney(betAmount);
-            totalBet += betAmount;
-        }
-        isReady = true;
-        return names;
-    }
-
-    public static void startRace(String[] betters) {
-        if (!isReady) {
-            System.out.println("Place the bets first!");
-            return;
-        }
-        Random rand = new Random();
-        int winner = rand.nextInt(10);
-        List<Racers> winnerList = new ArrayList<>();
-        //System.out.println("secret: " + winner);
-        for (String name : betters) {
-            Racers curr = findPersonByName(Registration.racerList, name);
-            System.out.print(curr.getName());
-            curr.setGuess(askForInt(" who do you bet on?\n"));
-            if (curr.getGuess() == winner) {
-                winnerList.add(curr);
+        int betAmount = askForInt("Enter bet amount:");
+        foundUsers = findUsers(Registration.userTable, names);
+        if (foundUsers == null) {
+            System.out.println("No users found");
+            return false;
+        } else {
+            if (!usersSufficientBalanceCheck(foundUsers, betAmount)) {
+                System.out.println("Someone doesn't have enough money for this.\nReturning to menu.");
+                return false;
             }
         }
 
-        for (Racers racer : winnerList) {
-            int earning = totalBet / winnerList.size();
-            System.out.println(racer.getName() + " you won! You will receive " + earning);
-            racer.addMoney(earning);
+        for (Users user : foundUsers) {
+            user.subtractMoney(betAmount);
+            totalBet += betAmount;
+        }
+        System.out.println("Bets are placed! You may start the race!");
+
+        return true;
+    }
+
+    public static boolean startRace() {
+        Random rand = new Random();
+        int winner = rand.nextInt(10);
+        System.out.println(winner + " but shhhhh!");
+        Map<Integer, List<Users>> winnerList = new HashMap<>();
+
+        for (Users user : foundUsers) {
+            System.out.print(user.getName());
+            Integer guess = askForInt(" enter your guess:");
+            if (!winnerList.containsKey(guess)) {
+                winnerList.put(guess, new ArrayList<>());
+                winnerList.get(guess).add(user);
+            } else {
+                winnerList.get(guess).add(user);
+            }
+        }
+
+        if (!winnerList.containsKey(winner)) {
+            System.out.println("No users won. Winning number was " + winner);
+        } else {
+            int howManyWinners = winnerList.get(winner).size();
+            int prize = totalBet / howManyWinners;
+            for (Users user : winnerList.get(winner)) {
+                System.out.print(user.getName() + " ");
+                user.addMoney(prize);
+            }
+            if (howManyWinners == 1) {
+                System.out.println("wins " + prize + "!");
+            } else {
+                System.out.print("each win " + prize + "!");
+            }
         }
 
         totalBet = 0;
-        isReady = false;
+        foundUsers.clear();
+
+        return false;
     }
 
-    public static Racers findPersonByName(List<Racers> users, String name) {
-        for (Racers user : users) {
-            if (user.getName().equalsIgnoreCase(name)) {
-                return user;
+    public static boolean usersSufficientBalanceCheck(List<Users> users, int amount) {
+        for (Users user : users) {
+            if (!user.doIHaveEnoughMoney(amount)) {
+                return false;
             }
         }
-        return null;
+        return true;
+    }
+
+    public static List<Users> findUsers(Map<String, Users> users, String[] names) {
+        List<Users> foundUsers = new ArrayList<>();
+        for (String name : names) {
+            if (users.containsKey(name)) {
+                foundUsers.add(users.get(name));
+            } else {
+                System.out.println("Check names again!");
+                return null;
+            }
+        }
+        return foundUsers;
     }
 
     public static int menuShowAndPick() {
-        System.out.println("""
+        System.out.print("""
                 Menu:
                 1. Register Users
                 2. Show Users and Balances
